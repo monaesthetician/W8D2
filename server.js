79c78a13efb56a47fd5a34c8a1acf65bf7d3ed18
Mona <emonearth915@gmail.com>
2021-05-19 20:27:53 -0400

    nodemon app

--- server.js
+++ server.js
@@ -28,16 +28,30 @@
 // homework
 const http = require('http');
 const fs = require('fs');
-
+const _ = require ('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+
+        // lodash
+        const num = _.random(0, 20);
+        console.log(num);
+    
+
+    
+
+     const greet = _.once(() =>{
+         console.log('hello');
+     });
+     greet();
+     greet();
+    // console.log(req.url, req.method);
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
     let path = './views/';
     switch(req.url) {
+
         case '/':
            path += 'index.html';
            res.statusCode = 200;
@@ -47,18 +61,19 @@ const server = http.createServer((req, res) => {
            path += 'about.html';
            res.statusCode = 200;
            break;
-           
-        case '/about-me':
-           res.statusCode = 301;
-           res.setHeader('Location', '/about');
-           res.end();
-           break;
-        default:
-            path += '404.html';
-            res.statusCode = 404;
-            break; 
-    }
 
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            // break;
+         default:
+             path += '404.html';
+             res.statusCode = 404;
+             break; 
+
+    }
+     
     // send an html file
     fs.readFile(path, (err, data) => {
         if (err) {
@@ -66,8 +81,7 @@ const server = http.createServer((req, res) => {
              res.end();
        } else {
         
-        //    res.write(data);
-             res.statusCode =200;
+           res.write(data);
              res.end();
          }
     })
